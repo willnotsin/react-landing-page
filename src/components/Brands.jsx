018b938a8f619c42ef56858aedc6fd2af772b874
willnotsin <wedafe45@gmail.com>
2022-10-03 22:24:38 +0100

    add the brand component

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+//import data
+import { overview } from '../data'
 
 const Brands = () => {
+  const {brands} = overview
   return (
-    <div>Brands</div>
+    <section className='py-9'>
+      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-5">
+        {/* brands */}
+        {brands.map((item, index)=> {
+          const {image, delay} = item
+          return(
+            <div key={index} data-aos="zoom-in-up" data-aos-delay={delay}>
+              {/* brand image */}
+              <img src={image} alt="" />
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
