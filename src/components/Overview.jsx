054c7b97a6785996be6f594b64e8cd6aeca00971
willnotsin <wedafe45@gmail.com>
2022-10-03 21:58:03 +0100

    overview added

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+//import data 
+import {overview} from '../data'
 
 const Overview = () => {
+  const {productImg} = overview
   return (
-    <div>Overview</div>
+    <section className='lg:min-h-[300px] bg-overview bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
+      <div className="container mx-auto flex justify-end overflow-hidden">
+        <img src={productImg} alt="" data-aos='flip-up' data-aos-offset='200' />
+      </div>
+    </section>
   )
 }
 
